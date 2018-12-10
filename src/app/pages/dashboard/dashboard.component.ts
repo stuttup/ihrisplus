4b2fc4d16023d7a92a223b2dbf313bc0a194bf01
cheikhou <niangcheikhou@gmail.com>
2018-12-11 00:35:09 +0100

    minor fix

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,23 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'ngx-dashboard',
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   constructor(private http: HttpClient) { }
 
-  testData: any
+  testData: any;
 
   testApi(){
     this.http.get('http://localhost:5000/api/resource/1').subscribe((data) => {
-      this.testData = data['timestamp']
-    })
+      this.testData = data['timestamp'];
+    });
   }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.testApi()
+    this.testApi();
   }
 }
