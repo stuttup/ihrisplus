6b3156f714710029377cd618c08d240d0c866cf1
cheikhou <niangcheikhou@gmail.com>
2018-12-11 00:31:42 +0100

    added flask.service.ts

---
+++ src/app/@core/data/flask.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class FlaskApiService {
+  constructor(private http: HttpClient) { }
+
+  apiUrl = 'localhost:5000/api/resource/1'
+
+  getResource(){
+      return this.http.get(this.apiUrl)
+  }
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'ngx-dashboard',
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
+  constructor(private http: HttpClient) { }
+
+  testData: any
+
+  testApi(){
+    this.http.get('http://localhost:5000/api/resource/1').subscribe((data) => {
+      this.testData = data['timestamp']
+    })
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.testApi()
+  }
 }
