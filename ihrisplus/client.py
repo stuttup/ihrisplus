67915570768a651f62dde3ceff27fb85f129d62e
cheikhou <niangcheikhou@gmail.com>
2018-12-11 00:28:54 +0100

    added flask app

---
+++ ihrisplus/client.py
@@ -0,0 +1,7 @@
+# coding: utf8
+import os
+from flask import Blueprint
+
+
+client_bp = Blueprint('client_app', __name__, url_prefix='', static_url_path='',
+                static_folder='../dist/static/', template_folder='../dist')
--- ihrisplus/database.py
+++ ihrisplus/database.py
@@ -6,9 +6,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 url = os.getenv('DATABASE_URL')
+
 
 engine = create_engine(url)
 Session = sessionmaker(bind=engine)
